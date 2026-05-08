1961c711a51b7709391d00e04674682aea08bf2f
gabrielmazui <gabi010706@gmail.com>
2026-05-08 00:35:49 -0300

    project 4

--- 04/Fill/Fill.asm
+++ 04/Fill/Fill.asm
@@ -9,3 +9,61 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+@i
+M=0 // i = 0
+
+(LOOP)
+@KBD
+D=M // D = RAM[24576]
+
+@WHITE
+D;JEQ // D == 0
+@BLACK
+0;JMP // D != 0
+
+(LOOPCOLOR)
+@SCREEN
+D=A   // D = 16384
+@i
+D=D+M   // D = i + SCREEN
+@KBD
+D=A-D    // D = 24576 - (SCREEN + i)
+@END
+D;JEQ    // D == 0
+
+@SCREEN
+D=A   // D = 16384
+@i
+D=D+M   // D = i + SCREEN
+@addr
+M=D // addr = I + SCREEN
+@color 
+D=M // D = color
+@addr
+A=M // RAM[I+SCREEN]
+M=D // RAM[I+SCREEN] = color
+
+@i
+M=M+1 // i = i+1
+@LOOPCOLOR
+0;JMP
+
+(END)
+@i
+M=0 // i = 0
+@LOOP
+0;JMP
+
+
+(WHITE)
+@color
+M=0
+@LOOPCOLOR
+0;JMP
+
+(BLACK)
+@color
+M=-1
+@LOOPCOLOR
+0;JMP
--- 04/Mult/Mult.asm
+++ 04/Mult/Mult.asm
@@ -7,4 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// sum = 0
+@sum
+M=0
+
+// i = 1
+@i
+M=1
+
+// i <= R0 
+(LOOP)
+@i
+D=M // D = i
+@R0
+D=D-M   // D = i - R0
+@END
+D;JGT // (I - R0) > 0
+
+// sum = sum + R1
+@R1
+D=M
+@sum
+M=D+M
+
+// i = i + 1
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+// R2 = SUM
+@sum
+D=M
+@R2
+M=D
